e3a0c3d0823b9f3e45ad8cde6e20faeba4f3e759
Andrew Tobin <andrew@andrewtobin.org>
2015-09-03 18:49:25 +1000

    Creating Test Start and Member Test Init

---
+++ Library.Tests/MemberTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Entities;
+using Library.Interfaces.Entities;
+using Xunit;
+
+namespace Library.Tests
+{
+    public class MemberTests
+    {
+        [Fact]
+        public void CanCreateANewMember()
+        {
+            var member = new Member();
+
+            Assert.NotNull(member);
+        }
+
+        [Fact]
+        public void MemberImplementsIMemberInterface()
+        {
+            var member = new Member() as IMember;
+
+            Assert.NotNull(member);
+        }
+
+    }
+}

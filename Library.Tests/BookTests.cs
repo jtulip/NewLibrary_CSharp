83af663e71883f6152a51160c26a0f2c55c0bf10
Andrew Tobin <andrew@andrewtobin.org>
2015-09-21 19:21:02 +1000

    Adding Book tests and Can Create Book

---
+++ Library.Tests/BookTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Entities;
+using Xunit;
+
+namespace Library.Tests
+{
+    [Trait("Category", "Book Tests")]
+    public class BookTests
+    {
+        [Fact]
+        public void CanCreateANewBook()
+        {
+            var book = new Book();
+
+            Assert.NotNull(book);
+        }
+    }
+}

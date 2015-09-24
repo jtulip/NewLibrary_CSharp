cbbb7ac907b6af27d235c60d46ed894ea9cbf2e4
Andrew Tobin <andrew@andrewtobin.org>
2015-09-24 18:39:14 +1000

    Can make a loan helper.

--- Library.Tests/LoanHelperTests.cs
+++ Library.Tests/LoanHelperTests.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library.Entities;
+using Library.Interfaces.Entities;
+using NSubstitute;
 using Xunit;
 
 namespace Library.Tests
@@ -10,5 +13,12 @@ namespace Library.Tests
     [Trait("Category", "Loan Tests")]
     public class LoanHelperTests
     {
+        [Fact]
+        public void CanMakeLoan()
+        {
+            var helper = new LoanHelper();
+
+            Assert.NotNull(helper);
+        }
     }
 }

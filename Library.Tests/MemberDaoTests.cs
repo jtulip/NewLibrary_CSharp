4611f1d762ecfbcd08ba4cebe7de504efe612221
Andrew Tobin <andrew@andrewtobin.org>
2015-09-24 14:33:00 +1000

    Can create MemberDao

--- Library.Tests/MemberDaoTests.cs
+++ Library.Tests/MemberDaoTests.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library.Daos;
+using Library.Interfaces.Entities;
+using NSubstitute;
 using Xunit;
 
 namespace Library.Tests
@@ -10,6 +13,14 @@ namespace Library.Tests
     [Trait("Category", "Member Tests")]
     public class MemberDaoTests
     {
+        [Fact]
+        public void CanCreateMemberDao()
+        {
+            var helper = Substitute.For<IMemberHelper>();
 
+            var memberDao = new MemberDao(helper);
+
+            Assert.NotNull(memberDao);
+        }
     }
 }
---
+++ Library/Daos/MemberDao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Interfaces.Entities;
+
+namespace Library.Daos
+{
+    public class MemberDao
+    {
+        public MemberDao(IMemberHelper helper)
+        {
+        }
+    }
+}

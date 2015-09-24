3be1d345988626bbe0f00bdb0b112648be260c84
Andrew Tobin <andrew@andrewtobin.org>
2015-09-24 11:11:09 +1000

    Can create BookDao

--- Library.Tests/BookDaoTests.cs
+++ Library.Tests/BookDaoTests.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Library.Daos;
+using Library.Interfaces.Entities;
+using NSubstitute;
 using Xunit;
 
 namespace Library.Tests
@@ -10,5 +14,14 @@ namespace Library.Tests
     [Trait("Category", "Book Tests")]
     public class BookDaoTests
     {
+        [Fact]
+        public void CanCreateBookDao()
+        {
+            var helper = Substitute.For<IBookHelper>();
+
+            var bookDao = new BookDao(helper);
+
+            Assert.NotNull(bookDao);
+        }
     }
 }
---
+++ Library/Daos/BookDao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Interfaces.Entities;
+
+namespace Library.Daos
+{
+    public class BookDao
+    {
+        public BookDao(IBookHelper helper)
+        {
+            
+        }
+    }
+}

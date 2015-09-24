96e2d73fcaf004a8cda87af1db509a11c718f730
Andrew Tobin <andrew@andrewtobin.org>
2015-09-24 15:05:19 +1000

    Can add a member.

--- Library.Tests/MemberDaoTests.cs
+++ Library.Tests/MemberDaoTests.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Library.Daos;
+using Library.Entities;
+using Library.Interfaces.Daos;
 using Library.Interfaces.Entities;
 using NSubstitute;
 using Xunit;
@@ -31,6 +33,55 @@ namespace Library.Tests
             Assert.Equal("Helper must be provided when creating MemberDao", ex.Message);
         }
 
+        [Fact]
+        public void MemberDaoImplementsIMemberDAOInterface()
+        {
+            var helper = Substitute.For<IMemberHelper>();
 
+            var memberDao = new MemberDao(helper);
+
+            Assert.IsAssignableFrom<IMemberDAO>(memberDao);
+
+            var typedMember = memberDao as IMemberDAO;
+
+            Assert.NotNull(typedMember);
+        }
+
+        [Fact]
+        public void CanAddMember()
+        {
+            var helper = Substitute.For<IMemberHelper>();
+
+            var memberDao = new MemberDao(helper);
+
+            var firstName = "first";
+            var lastName = "last";
+            var contactPhone = "phone";
+            var emailAddress = "email address";
+
+            // Uses Helper to create a new member with a unique member ID.
+            // Adds the member to a collection of members and returns new member.
+            Assert.Equal(0, memberDao.MemberList.Count);
+
+            // Tell the mock what to return when it is called.
+            helper.MakeMember(firstName, lastName, contactPhone, emailAddress, Arg.Any<int>()).Returns(new Member(firstName, lastName, contactPhone, emailAddress, 1));
+
+            var result = memberDao.AddMember(firstName, lastName, contactPhone, emailAddress);
+
+            // Assert that the mock's MakeMember method was called.
+            helper.Received().MakeMember(firstName, lastName, contactPhone, emailAddress, Arg.Any<int>());
+
+            Assert.NotNull(result);
+            Assert.Equal(firstName, result.FirstName);
+            Assert.Equal(lastName, result.LastName);
+            Assert.Equal(contactPhone, result.ContactPhone);
+            Assert.Equal(emailAddress, result.EmailAddress);
+
+            Assert.Equal(1, memberDao.MemberList.Count);
+
+            var member = memberDao.MemberList[0];
+
+            Assert.Equal(member, result);
+        }
     }
 }
--- Library/Daos/MemberDao.cs
+++ Library/Daos/MemberDao.cs
@@ -3,15 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library.Interfaces.Daos;
 using Library.Interfaces.Entities;
 
 namespace Library.Daos
 {
-    public class MemberDao
+    public class MemberDao: IMemberDAO
     {
         public MemberDao(IMemberHelper helper)
         {
             if(helper == null) throw new ArgumentException("Helper must be provided when creating MemberDao");
+
+            _helper = helper;
+
+            this.MemberList = new List<IMember>();
+        }
+
+        private IMemberHelper _helper { get; }
+        public List<IMember> MemberList { get; }
+
+        public IMember AddMember(string firstName, string lastName, string contactPhone, string emailAddress)
+        {
+            var member = _helper.MakeMember(firstName, lastName, contactPhone, emailAddress, 1);
+
+            this.MemberList.Add(member);
+
+            return member;
+        }
+
+        public IMember GetMemberByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public List<IMember> FindMembersByLastName(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IMember> FindMembersByEmailAddress(string emailAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IMember> FindMembersByNames(string firstName, string lastName)
+        {
+            throw new NotImplementedException();
         }
     }
 }

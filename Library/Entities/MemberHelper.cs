8b4e206744b8a31572814c81adce7d7380761167
Andrew Tobin <andrew@andrewtobin.org>
2015-09-24 19:02:05 +1000

    Implements interface

--- Library/Entities/MemberHelper.cs
+++ Library/Entities/MemberHelper.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library.Interfaces.Entities;
 
 namespace Library.Entities
 {
-    public class MemberHelper
+    public class MemberHelper: IMemberHelper
     {
+        public IMember MakeMember(string firstName, string lastName, string contactPhone, string emailAddress, int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

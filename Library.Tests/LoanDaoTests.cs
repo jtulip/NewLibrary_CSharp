fd1dbe19577f5fb782a6343f4100492612c5bf86
Andrew Tobin <andrew@andrewtobin.org>
2015-09-24 16:49:07 +1000

    Can create LoanDAO

---
+++ Library.Tests/LoanDaoTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Daos;
+using Library.Interfaces.Entities;
+using NSubstitute;
+using Xunit;
+
+namespace Library.Tests
+{
+    [Trait("Category", "Loan Tests")]
+    public class LoanDaoTests
+    {
+        [Fact]
+        public void CanCreateLoanDao()
+        {
+            var helper = Substitute.For<ILoanHelper>();
+
+            var loanDao = new LoanDao(helper);
+
+            Assert.NotNull(loanDao);
+        }
+    }
+}

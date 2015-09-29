b02b59d762f3518a94dbe731478e1983bcfafea5
Andrew Tobin <andrew@andrewtobin.org>
2015-09-30 09:42:28 +1000

    Have overdue loans returns true if any overdue

--- Library.Tests/Integration/Entity/MemberIntegration.cs
+++ Library.Tests/Integration/Entity/MemberIntegration.cs
@@ -27,6 +27,33 @@ namespace Library.Tests.Integration.Entity
             Assert.False(member.HasOverDueLoans);
         }
 
+        [Fact]
+        public void HasOverdueLoansReturnsTrueIfAnyLoanIsOverdue()
+        {
+            var book = new Book("author", "title", "call number", 1);
+
+            var member = new Member("first", "last", "phone", "email", 1);
+
+            // Add a single loan that is not overdue.
+            var loan = new Loan(book, member, DateTime.Today, DateTime.Today.AddDays(14)) { State = LoanState.CURRENT };
+
+            member.AddLoan(loan);
+
+            // Add a second loan that is overdue.
+            var secondLoan = new Loan(book, member, DateTime.Today, DateTime.Today.AddDays(7)) { State = LoanState.CURRENT };
+
+            member.AddLoan(secondLoan);
+
+            // Check if overdue
+
+            foreach (var l in member.Loans)
+            {
+                l.CheckOverDue(DateTime.Today.AddDays(10));
+            }
+
+            Assert.True(member.HasOverDueLoans);
+        }
+
     }
 }
 

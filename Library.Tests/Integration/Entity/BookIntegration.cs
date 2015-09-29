da54aeac4c4b3164e1ede710a2aa3cdd74727744
Andrew Tobin <andrew@andrewtobin.org>
2015-09-30 09:21:36 +1000

    Get loan from book returns null if book is not on loan

--- Library.Tests/Integration/Entity/BookIntegration.cs
+++ Library.Tests/Integration/Entity/BookIntegration.cs
@@ -64,5 +64,38 @@ namespace Library.Tests.Integration.Entity
             Assert.Equal(loan, loanRetrieved);
         }
 
+        [Fact]
+        public void GetLoanFromBookReturnsNullIfBookIsNotON_LOAN()
+        {
+            var book = new Book("author", "title", "call number", 1);
+            var member = new Member("first", "last", "phone", "email", 1);
+
+            var loan = new Loan(book, member, DateTime.Today, DateTime.Today.AddDays(7));
+
+            // Testing the getter on the book.
+            var loanRetrieved = book.Loan;
+
+            // Make sure the book returns null when no loan associated.
+            Assert.Null(loanRetrieved);
+
+            // Associate the book with the loan.
+            book.Borrow(loan);
+
+            // Make sure the loan comes back if it is on loan.
+            book.State = BookState.ON_LOAN;
+
+            loanRetrieved = book.Loan;
+
+            // Make sure the loan retrieved is the same one loaned out.
+            Assert.Equal(loan, loanRetrieved);
+
+            // Set the loan state to not ON_LOAN.
+            book.State = BookState.AVAILABLE;
+
+            // Make sure null is returned if book is not ON_LOAN.
+            loanRetrieved = book.Loan;
+
+            Assert.Null(loanRetrieved);
+        }
     }
 }

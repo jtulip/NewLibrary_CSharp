8416248d988fb9bca91001bfa82813323d1cf7b6
Andrew Tobin <andrew@andrewtobin.org>
2015-09-29 10:10:05 +1000

    When member created borrowing allowed

---
+++ Library.Tests/UnitTests/MemberStateTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Entities;
+using Library.Interfaces.Entities;
+using Xunit;
+
+namespace Library.Tests.UnitTests
+{
+    [Trait("Category", "Member Tests")]
+    public class MemberStateTests
+    {
+        [Fact]
+        public void WhenMemberCreatedShouldBeBorrowingAllowed()
+        {
+            var member = new Member("firstName", "lastName", "contactPhone", "emailAddress", 1);
+
+            Assert.Equal(MemberState.BORROWING_ALLOWED, member.State);
+        }
+    }
+}

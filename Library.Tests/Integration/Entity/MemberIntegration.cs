75fd0c12c8e1c8bb8b6db27266c4af77f45ce7a9
Andrew Tobin <andrew@andrewtobin.org>
2015-09-30 09:35:42 +1000

    Has overdue loans is false if no loan is overdue

--- Library.Tests/Integration/Entity/MemberIntegration.cs
+++ Library.Tests/Integration/Entity/MemberIntegration.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library.Entities;
+using Library.Interfaces.Entities;
+using Xunit;
 
 namespace Library.Tests.Integration.Entity
 {
+    [Trait("Category", "Member Tests")]
     public class MemberIntegration
     {
+        [Fact]
+        public void HasOverdueLoansReturnsFalseIfNoLoanIsOverdue()
+        {
+            var book = new Book("author", "title", "call number", 1);
+
+            var member = new Member("first", "last", "phone", "email", 1);
+
+            // Add a single loan that is not overdue.
+            var loan = new Loan(book, member, DateTime.Today, DateTime.Today.AddDays(7));
+
+            member.Loans.Add(loan);
+
+            Assert.False(member.HasOverDueLoans);
+        }
+
     }
 }
+

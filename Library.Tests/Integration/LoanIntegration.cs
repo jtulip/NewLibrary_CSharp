36a12c986500c976e617e6e1de1602ded01177e3
Andrew Tobin <andrew@andrewtobin.org>
2015-09-29 17:49:51 +1000

    Can commit loan

--- Library.Tests/Integration/LoanIntegration.cs
+++ Library.Tests/Integration/LoanIntegration.cs
@@ -14,6 +14,7 @@ namespace Library.Tests.Integration
     [Trait("Category", "Loan Tests")]
     public class LoanIntegration
     {
+        [Fact]
         public void CanCreateLoan()
         {
             ILoanHelper loanHelper = new LoanHelper();
@@ -42,6 +43,37 @@ namespace Library.Tests.Integration
             Assert.Equal(member, loan.Borrower);
 
             Assert.Equal(0, loan.ID);
+        }
+
+        [Fact]
+        public void CanCommitLoan()
+        {
+            ILoanHelper loanHelper = new LoanHelper();
+            ILoanDAO loanDao = new LoanDao(loanHelper);
+
+            IMemberHelper memberHelper = new MemberHelper();
+            IMemberDAO memberDao = new MemberDao(memberHelper);
+
+            IBookHelper bookHelper = new BookHelper();
+            IBookDAO bookDao = new BookDao(bookHelper);
+
+            var borrowDate = DateTime.Today;
+            var dueDate = DateTime.Today.AddDays(7);
+
+            var member = memberDao.AddMember("Jim", "Tulip", "csu phone", "jim@example.com");
+
+            var book = bookDao.AddBook("Jim Tulip", "Adventures in Programming", "call number");
+
+            var loan = loanDao.CreateLoan(member, book, borrowDate, dueDate);
+
+            loanDao.CommitLoan(loan);
+
+            Assert.NotNull(loan);
+
+            Assert.Equal(1, loanDao.LoanList.Count);
+            Assert.Equal(loan, loanDao.LoanList[0]);
+
+            Assert.NotEqual(0, loan.ID);
         }
 
     }

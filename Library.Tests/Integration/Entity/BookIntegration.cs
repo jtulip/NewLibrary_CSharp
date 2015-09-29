55670a17835cb7d10b1f57fecb29c56d1a6550bd
Andrew Tobin <andrew@andrewtobin.org>
2015-09-30 09:24:46 +1000

    Returning book sets state to available

--- Library.Tests/Integration/Entity/BookIntegration.cs
+++ Library.Tests/Integration/Entity/BookIntegration.cs
@@ -139,5 +139,28 @@ namespace Library.Tests.Integration.Entity
             Assert.Null(book.Loan);
             Assert.Equal(BookState.DAMAGED, book.State);
         }
+
+        [Fact]
+        public void ReturningBookSetsStateToAvailable()
+        {
+            var book = new Book("author", "title", "call number", 1);
+            var member = new Member("first", "last", "phone", "email", 1);
+
+            var loan = new Loan(book, member, DateTime.Today, DateTime.Today.AddDays(7));
+
+            book.Borrow(loan);
+
+            // Set book state to ON_LOAN - affected by GetLoanFromBookReturnsNullIfBookIsNotON_LOAN()
+            book.State = BookState.ON_LOAN;
+
+            Assert.Equal(loan, book.Loan);
+
+            // Set damaged flag to false.
+            book.ReturnBook(false);
+
+            Assert.Null(book.Loan);
+            Assert.Equal(BookState.AVAILABLE, book.State);
+        }
+
     }
 }

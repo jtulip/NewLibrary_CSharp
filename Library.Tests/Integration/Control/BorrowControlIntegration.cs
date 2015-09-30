a861f8eb2c0d8c346a2f41b7451478efc5de6548
Andrew Tobin <andrew@andrewtobin.org>
2015-09-30 22:30:13 +1000

    Create control throws on null arguments

---
+++ Library.Tests/Integration/Control/BorrowControlIntegration.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Controllers.Borrow;
+using Library.Controls.Borrow;
+using Library.Daos;
+using Library.Entities;
+using Library.Interfaces.Controllers.Borrow;
+using Library.Interfaces.Daos;
+using Library.Interfaces.Entities;
+using Library.Interfaces.Hardware;
+using NSubstitute;
+using Xunit;
+
+namespace Library.Tests.Integration.Control
+{
+    [Trait("Category", "BorrowControl Tests")]
+
+    public class BorrowControlIntegration
+    {
+        public BorrowControlIntegration()
+        {
+            _bookDao = new BookDao(new BookHelper());
+            _loanDao = new LoanDao(new LoanHelper());
+            _memberDao = new MemberDao(new MemberHelper());
+
+            // Only mocking UI concerns
+            _display = Substitute.For<IDisplay>();
+            _reader = Substitute.For<ICardReader>();
+            _scanner = Substitute.For<IScanner>();
+            _printer = Substitute.For<IPrinter>();
+        }
+
+        private IDisplay _display;
+        private ICardReader _reader;
+        private IScanner _scanner;
+        private IPrinter _printer;
+        private IBookDAO _bookDao;
+        private ILoanDAO _loanDao;
+        private IMemberDAO _memberDao;
+
+        [WpfFact]
+        public void CreateControlThrowsExceptionOnNullArguments()
+        {
+            var ex = Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    var ctrl = new BorrowController(null, _reader, _scanner, _printer, _bookDao, _loanDao,
+                        _memberDao);
+                });
+
+            Assert.Equal("Display object was not provided to begin the application", ex.Message);
+
+            ex = Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    var ctrl = new BorrowController(_display, null, _scanner, _printer, _bookDao, _loanDao,
+                        _memberDao);
+                });
+
+            Assert.Equal("Reader object was not provided to begin the application", ex.Message);
+
+            ex = Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    var ctrl = new BorrowController(_display, _reader, null, _printer, _bookDao, _loanDao,
+                        _memberDao);
+                });
+
+            Assert.Equal("Scanner object was not provided to begin the application", ex.Message);
+
+            ex = Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    var ctrl = new BorrowController(_display, _reader, _scanner, null, _bookDao, _loanDao,
+                        _memberDao);
+                });
+
+            Assert.Equal("Printer object was not provided to begin the application", ex.Message);
+
+            ex = Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    var ctrl = new BorrowController(_display, _reader, _scanner, _printer, null, _loanDao,
+                        _memberDao);
+                });
+
+            Assert.Equal("BookDAO object was not provided to begin the application", ex.Message);
+
+            ex = Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    var ctrl = new BorrowController(_display, _reader, _scanner, _printer, _bookDao, null,
+                        _memberDao);
+                });
+
+            Assert.Equal("LoanDAO object was not provided to begin the application", ex.Message);
+
+            ex = Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    var ctrl = new BorrowController(_display, _reader, _scanner, _printer, _bookDao, _loanDao,
+                        null);
+                });
+
+            Assert.Equal("MemberDAO object was not provided to begin the application", ex.Message);
+        }
+
+
+        public void Dispose()
+        {
+            _display = null;
+            _reader = null;
+            _scanner = null;
+            _printer = null;
+            _bookDao = null;
+            _loanDao = null;
+            _memberDao = null;
+        }
+    }
+}

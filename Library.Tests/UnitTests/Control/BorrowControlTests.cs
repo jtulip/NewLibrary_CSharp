af2ba2c542a60168af36fb5c3f68d8a8ce06f1eb
Andrew Tobin <andrew@andrewtobin.org>
2015-09-30 13:57:48 +1000

    Initialise controller sets state to initialized

--- Library.Tests/UnitTests/Control/BorrowControlTests.cs
+++ Library.Tests/UnitTests/Control/BorrowControlTests.cs
@@ -172,96 +172,106 @@ namespace Library.Tests.UnitTests.Control
         }
 
         [WpfFact]
-        public void BBUC_OP1_BeginUseCase()
+        public void InitialiseControllerSetsStateToInitialized()
         {
-            // Must be done on an STA thread for WPF
-            var mockThis = Substitute.For<IBorrowListener>();
-
             var ctrl = new BorrowController(_display, _reader, _scanner, _printer, _bookDao, _loanDao, _memberDao);
 
-            _reader.Received().Listener = ctrl;
-            _scanner.Received().Listener = ctrl;
-
-            Assert.Equal(ctrl, _reader.Listener);
-            Assert.Equal(ctrl, _scanner.Listener);
-
-            Assert.Equal(EBorrowState.CREATED, ctrl._state);
-
-            //ctrl._ui = new BorrowControl(mockThis);
-
             ctrl.initialise();
-
-            //var borrowControl = (BorrowControl)ctrl._ui;
-
-            //Assert.Equal(mockThis, borrowControl._listener);
-            //Assert.Equal(4, borrowControl._controlDict.Count);
-
-            //var swipeCardPanel = borrowControl._controlDict.Values.Single(c => c is SwipeCardControl);
-
-            //Assert.Equal(swipeCardPanel, borrowControl._currentControl);
-            //Assert.True(((SwipeCardControl)swipeCardPanel).IsEnabled);
-            //Assert.True(((SwipeCardControl)swipeCardPanel).cancelButton.IsEnabled);
-
-            Assert.True(_reader.Enabled);
-            Assert.False(_scanner.Enabled);
 
             Assert.Equal(EBorrowState.INITIALIZED, ctrl._state);
         }
 
-        [WpfFact]
-        public void BBUC_OP2_SwipeBorrowerCard()
-        {
-            var mockThis = Substitute.For<IBorrowListener>();
+        //[WpfFact]
+        //public void BBUC_OP1_BeginUseCase()
+        //{
+        //    // Must be done on an STA thread for WPF
+        //    var mockThis = Substitute.For<IBorrowListener>();
 
-            var ctrl = new BorrowController(_display, _reader, _scanner, _printer, _bookDao, _loanDao, _memberDao);
+        //    var ctrl = new BorrowController(_display, _reader, _scanner, _printer, _bookDao, _loanDao, _memberDao);
 
-            _reader.Received().Listener = ctrl;
-            _scanner.Received().Listener = ctrl;
+        //    _reader.Received().Listener = ctrl;
+        //    _scanner.Received().Listener = ctrl;
 
-            Assert.Equal(ctrl, _reader.Listener);
-            Assert.Equal(ctrl, _scanner.Listener);
+        //    Assert.Equal(ctrl, _reader.Listener);
+        //    Assert.Equal(ctrl, _scanner.Listener);
 
-            Assert.Equal(EBorrowState.CREATED, ctrl._state);
+        //    Assert.Equal(EBorrowState.CREATED, ctrl._state);
 
-            ctrl._ui = new BorrowControl(mockThis);
+        //    //ctrl._ui = new BorrowControl(mockThis);
 
-            ctrl.initialise();
+        //    ctrl.initialise();
 
-            //var borrowControl = (BorrowControl)ctrl._ui;
+        //    //var borrowControl = (BorrowControl)ctrl._ui;
 
-            //Assert.Equal(mockThis, borrowControl._listener);
-            //Assert.Equal(4, borrowControl._controlDict.Count);
+        //    //Assert.Equal(mockThis, borrowControl._listener);
+        //    //Assert.Equal(4, borrowControl._controlDict.Count);
 
-            //var swipeCardPanel = borrowControl._controlDict.Values.Single(c => c is SwipeCardControl);
-            //var scanBookPanel = borrowControl._controlDict.Values.Single(c => c is ScanBookControl);
+        //    //var swipeCardPanel = borrowControl._controlDict.Values.Single(c => c is SwipeCardControl);
 
-            //Assert.Equal(swipeCardPanel, borrowControl._currentControl);
-            //Assert.True(((SwipeCardControl)swipeCardPanel).IsEnabled);
-            //Assert.True(((SwipeCardControl)swipeCardPanel).cancelButton.IsEnabled);
+        //    //Assert.Equal(swipeCardPanel, borrowControl._currentControl);
+        //    //Assert.True(((SwipeCardControl)swipeCardPanel).IsEnabled);
+        //    //Assert.True(((SwipeCardControl)swipeCardPanel).cancelButton.IsEnabled);
 
-            Assert.True(_reader.Enabled);
-            Assert.False(_scanner.Enabled);
+        //    Assert.True(_reader.Enabled);
+        //    Assert.False(_scanner.Enabled);
 
-            Assert.Equal(EBorrowState.INITIALIZED, ctrl._state);
+        //    Assert.Equal(EBorrowState.INITIALIZED, ctrl._state);
+        //}
 
-            var member = Substitute.For<IMember>();
+        //[WpfFact]
+        //public void BBUC_OP2_SwipeBorrowerCard()
+        //{
+        //    var mockThis = Substitute.For<IBorrowListener>();
 
-            _memberDao.GetMemberByID(1).Returns(member);
+        //    var ctrl = new BorrowController(_display, _reader, _scanner, _printer, _bookDao, _loanDao, _memberDao);
 
-            ctrl.cardSwiped(1);
+        //    _reader.Received().Listener = ctrl;
+        //    _scanner.Received().Listener = ctrl;
 
-            //_memberDao.Received().GetMemberByID(1);
+        //    Assert.Equal(ctrl, _reader.Listener);
+        //    Assert.Equal(ctrl, _scanner.Listener);
 
-            //Assert.False(_reader.Enabled);
-            //Assert.True(_scanner.Enabled);
+        //    Assert.Equal(EBorrowState.CREATED, ctrl._state);
 
-            ////Assert.False(((SwipeCardControl)swipeCardPanel).IsEnabled);
-            ////Assert.True(((ScanBookControl)scanBookPanel).IsEnabled);
-            ////Assert.True(((ScanBookControl)scanBookPanel).cancelButton.IsEnabled);
-            ////Assert.True(((ScanBookControl)scanBookPanel).completeButton.IsEnabled);
-            ////Assert.Equal(3, ((ScanBookControl)scanBookPanel).);
-            //Assert.Equal(EBorrowState.SCANNING_BOOKS, ctrl._state);
-        }
+        //    ctrl._ui = new BorrowControl(mockThis);
+
+        //    ctrl.initialise();
+
+        //    //var borrowControl = (BorrowControl)ctrl._ui;
+
+        //    //Assert.Equal(mockThis, borrowControl._listener);
+        //    //Assert.Equal(4, borrowControl._controlDict.Count);
+
+        //    //var swipeCardPanel = borrowControl._controlDict.Values.Single(c => c is SwipeCardControl);
+        //    //var scanBookPanel = borrowControl._controlDict.Values.Single(c => c is ScanBookControl);
+
+        //    //Assert.Equal(swipeCardPanel, borrowControl._currentControl);
+        //    //Assert.True(((SwipeCardControl)swipeCardPanel).IsEnabled);
+        //    //Assert.True(((SwipeCardControl)swipeCardPanel).cancelButton.IsEnabled);
+
+        //    Assert.True(_reader.Enabled);
+        //    Assert.False(_scanner.Enabled);
+
+        //    Assert.Equal(EBorrowState.INITIALIZED, ctrl._state);
+
+        //    var member = Substitute.For<IMember>();
+
+        //    _memberDao.GetMemberByID(1).Returns(member);
+
+        //    ctrl.cardSwiped(1);
+
+        //    //_memberDao.Received().GetMemberByID(1);
+
+        //    //Assert.False(_reader.Enabled);
+        //    //Assert.True(_scanner.Enabled);
+
+        //    ////Assert.False(((SwipeCardControl)swipeCardPanel).IsEnabled);
+        //    ////Assert.True(((ScanBookControl)scanBookPanel).IsEnabled);
+        //    ////Assert.True(((ScanBookControl)scanBookPanel).cancelButton.IsEnabled);
+        //    ////Assert.True(((ScanBookControl)scanBookPanel).completeButton.IsEnabled);
+        //    ////Assert.Equal(3, ((ScanBookControl)scanBookPanel).);
+        //    //Assert.Equal(EBorrowState.SCANNING_BOOKS, ctrl._state);
+        //}
 
 
         public void Dispose()

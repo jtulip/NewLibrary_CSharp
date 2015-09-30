aab311f6593aa169ecd1dad8d9b2459a373a65b6
Andrew Tobin <andrew@andrewtobin.org>
2015-09-30 12:38:10 +1000

    When loan is committed book is borrowed

--- Library.Tests/Integration/Entity/LoanIntegration.cs
+++ Library.Tests/Integration/Entity/LoanIntegration.cs
@@ -111,5 +111,24 @@ namespace Library.Tests.Integration.Entity
             Assert.Equal(LoanState.CURRENT, loan.State);
         }
 
+        [Fact]
+        public void CommitLoanCallsBookBorrow()
+        {
+            var book = new Book("author", "title", "call number", 1);
+            var member = new Member("first", "last", "phone", "email", 1);
+
+            DateTime borrowDate = DateTime.Today;
+            DateTime dueDate = DateTime.Today.AddDays(7);
+            var loanId = 1;
+
+            var loan = new Loan(book, member, borrowDate, dueDate);
+
+            // Call the commit and expect that it will call book.Borrow.
+            loan.Commit(loanId);
+
+            // When loan is committed, assert book.Borrow will be called with the loan.
+            Assert.Equal(BookState.ON_LOAN, book.State);
+        }
+
     }
 }

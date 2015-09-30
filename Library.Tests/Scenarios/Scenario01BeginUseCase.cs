09c326453d0b601da4daa52eb06fac9e1cc7fe89
Andrew Tobin <andrew@andrewtobin.org>
2015-09-30 22:54:54 +1000

    Scenario 01 Use Case

---
+++ Library.Tests/Scenarios/Scenario01BeginUseCase.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Controllers.Borrow;
+using Library.Daos;
+using Library.Entities;
+using Library.Interfaces.Controllers.Borrow;
+using Library.Interfaces.Hardware;
+using NSubstitute;
+using Xunit;
+
+namespace Library.Tests.Scenarios
+{
+    [Trait("Category", "Scenarios")]
+
+    public class Scenario01BeginUseCase
+    {
+        [WpfFact]
+        public void RunScenario()
+        {
+           var bookDao = new BookDao(new BookHelper());
+           var loanDao = new LoanDao(new LoanHelper());
+           var memberDao = new MemberDao(new MemberHelper());
+           
+           // Only mocking UI concerns
+           var display = Substitute.For<IDisplay>();
+           var reader = Substitute.For<ICardReader>();
+           var scanner = Substitute.For<IScanner>();
+           var printer = Substitute.For<IPrinter>();
+
+            var controller = new BorrowController(display, reader, scanner, printer,
+                bookDao, loanDao, memberDao);
+
+            // Test pre-conditions
+            Assert.NotNull(controller);
+
+            Assert.NotNull(bookDao);
+            Assert.NotNull(loanDao);
+            Assert.NotNull(memberDao);
+            Assert.NotNull(display);
+            Assert.NotNull(reader);
+            Assert.NotNull(scanner);
+            Assert.NotNull(printer);
+
+            Assert.Equal(controller._bookDAO, bookDao);
+            Assert.Equal(controller._loanDAO, loanDao);
+            Assert.Equal(controller._memberDAO, memberDao);
+            Assert.Equal(controller._display, display);
+            Assert.Equal(controller._reader, reader);
+            Assert.Equal(controller._scanner, scanner);
+            Assert.Equal(controller._printer, printer);
+
+            Assert.Equal(controller._reader.Listener, controller);
+            Assert.Equal(controller._scanner.Listener, controller);
+
+            Assert.Equal(EBorrowState.CREATED, controller._state);
+
+            // Run use case
+            controller.initialise();
+
+            // Test post-conditions
+            Assert.True(reader.Enabled);
+            Assert.True(!scanner.Enabled);
+            Assert.Equal(EBorrowState.INITIALIZED, controller._state);
+        }
+    }
+}

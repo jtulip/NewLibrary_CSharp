5b3748dc0be6dea9ca13057c02eec7768e1ebab9
Andrew Tobin <andrew@andrewtobin.org>
2015-09-30 11:39:20 +1000

    When book is borrowed, lost, then disposed should be set to disposed

--- Library.Tests/Integration/Entity/BookIntegration.cs
+++ Library.Tests/Integration/Entity/BookIntegration.cs
@@ -212,5 +212,22 @@ namespace Library.Tests.Integration.Entity
 
             Assert.Equal(BookState.LOST, book.State);
         }
+
+        [Fact]
+        public void WhenBookIsLostAndDisposedShouldBeDisposed()
+        {
+            var book = new Book("author", "title", "call number", 1);
+            var member = new Member("first", "last", "phone", "email", 1);
+
+            var loan = new Loan(book, member, DateTime.Today, DateTime.Today.AddDays(7));
+
+            book.Borrow(loan);
+
+            book.Lose();
+
+            book.Dispose();
+
+            Assert.Equal(BookState.DISPOSED, book.State);
+        }
     }
 }

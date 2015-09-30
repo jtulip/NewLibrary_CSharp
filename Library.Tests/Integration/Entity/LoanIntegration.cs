1dbbbbf05e5e22111f586518aae99445a4af6872
Andrew Tobin <andrew@andrewtobin.org>
2015-09-30 13:02:49 +1000

    When loan is current and overdue, should change to overdue

--- Library.Tests/Integration/Entity/LoanIntegration.cs
+++ Library.Tests/Integration/Entity/LoanIntegration.cs
@@ -381,5 +381,26 @@ namespace Library.Tests.Integration.Entity
             Assert.Equal(LoanState.CURRENT, loan.State);
         }
 
+        [Fact]
+        public void WhenLoanIsCurrentAndOverdueShouldBeOverdue()
+        {
+            var book = new Book("author", "title", "call number", 1);
+            var member = new Member("first", "last", "phone", "email", 1);
+
+            DateTime borrowDate = DateTime.Today;
+            DateTime dueDate = DateTime.Today.AddDays(7);
+
+            var loanId = 1;
+
+            var loan = new Loan(book, member, borrowDate, dueDate);
+
+            loan.Commit(loanId);
+
+            Assert.Equal(LoanState.CURRENT, loan.State);
+
+            loan.CheckOverDue(DateTime.Today.AddDays(14));
+
+            Assert.Equal(LoanState.OVERDUE, loan.State);
+        }
     }
 }

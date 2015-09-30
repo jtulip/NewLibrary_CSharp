59e6d1f1dd97ad43a7cad8d1bc83a29e5784c1c7
Andrew Tobin <andrew@andrewtobin.org>
2015-09-30 22:39:02 +1000

    Card restricted

--- Library.Tests/Integration/Control/BorrowControlIntegration.cs
+++ Library.Tests/Integration/Control/BorrowControlIntegration.cs
@@ -328,6 +328,55 @@ namespace Library.Tests.Integration.Control
             Assert.Equal(EBorrowState.SCANNING_BOOKS, ctrl._state);
         }
 
+        [WpfFact]
+        public void SwipeBorrowerCardRestricted()
+        {
+            var borrowDate = DateTime.Today;
+            var dueDate = DateTime.Today.AddDays(7);
+
+            var member = _memberDao.AddMember("Jim", "Tulip", "Phone", "Email");
+
+            var book = _bookDao.AddBook("Jim Tulip", "Adventures in Programming", "call number");
+
+            var loan = _loanDao.CreateLoan(member, book, borrowDate, dueDate);
+
+            _loanDao.CommitLoan(loan);
+
+            _loanDao.UpdateOverDueStatus(DateTime.Today.AddMonths(1));
+
+            var ctrl = new BorrowController(_display, _reader, _scanner, _printer, _bookDao, _loanDao, _memberDao);
+
+            // Set the UI to the mock so we can test
+            var borrowctrl = Substitute.For<ABorrowControl>();
+            ctrl._ui = borrowctrl;
+
+            ctrl.initialise();
+
+            //Test pre-conditions
+            Assert.True(ctrl._reader.Enabled);
+            Assert.Equal(ctrl, ctrl._reader.Listener);
+            Assert.NotNull(ctrl._memberDAO);
+            Assert.Equal(EBorrowState.INITIALIZED, ctrl._state);
+
+            ctrl.cardSwiped(member.ID);
+
+            _reader.Received().Enabled = false;
+            _scanner.Received().Enabled = false;
+
+            borrowctrl.Received().DisplayMemberDetails(member.ID, $"{member.FirstName} {member.LastName}", member.ContactPhone);
+
+            borrowctrl.Received().DisplayErrorMessage("Member has been restricted from borrowing");
+
+            foreach (var l in member.Loans)
+            {
+                borrowctrl.Received().DisplayExistingLoan(l.ToString());
+            }
+
+            Assert.Equal(member, ctrl._borrower);
+            Assert.True(!ctrl._reader.Enabled);
+            Assert.True(!ctrl._scanner.Enabled);
+            Assert.Equal(EBorrowState.BORROWING_RESTRICTED, ctrl._state);
+        }
 
 
         public void Dispose()

27165a15dac5a39c467dd2aa4b87d933219cfaa0
Andrew Tobin <andrew@andrewtobin.org>
2015-09-30 22:36:00 +1000

    Error if user has reached fine limit

--- Library.Tests/Integration/Control/BorrowControlIntegration.cs
+++ Library.Tests/Integration/Control/BorrowControlIntegration.cs
@@ -211,6 +211,41 @@ namespace Library.Tests.Integration.Control
             borrowctrl.Received().DisplayAtLoanLimitMessage();
         }
 
+        [WpfFact]
+        public void SwipeBorrowerCardShowErrorIfMemberHasReachedFinesLimit()
+        {
+            var borrowDate = DateTime.Today;
+            var dueDate = DateTime.Today.AddDays(7);
+
+            var member = _memberDao.AddMember("Jim", "Tulip", "Phone", "Email");
+
+            var book = _bookDao.AddBook("Jim Tulip", "Adventures in Programming", "call number");
+
+            var loan = _loanDao.CreateLoan(member, book, borrowDate, dueDate);
+
+            _loanDao.CommitLoan(loan);
+
+            member.AddFine(BookConstants.FINE_LIMIT + 1.00f);
+
+            var ctrl = new BorrowController(_display, _reader, _scanner, _printer, _bookDao, _loanDao, _memberDao);
+
+            // Set the UI to the mock so we can test
+            var borrowctrl = Substitute.For<ABorrowControl>();
+            ctrl._ui = borrowctrl;
+
+            ctrl.initialise();
+
+            //Test pre-conditions
+            Assert.True(ctrl._reader.Enabled);
+            Assert.Equal(ctrl, ctrl._reader.Listener);
+            Assert.NotNull(ctrl._memberDAO);
+            Assert.Equal(EBorrowState.INITIALIZED, ctrl._state);
+
+            ctrl.cardSwiped(member.ID);
+
+            borrowctrl.Received().DisplayOverFineLimitMessage(member.FineAmount);
+        }
+
 
         public void Dispose()
         {

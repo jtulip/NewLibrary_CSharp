8650affbfb36724959fa84b97c4a146a7d8ac5f1
Andrew Tobin <andrew@andrewtobin.org>
2015-10-01 23:28:52 +1000

    Scenario 03 - Scan books - Scan count equals loan limit

--- Library.Tests/Scenarios/Scenario03ScanBook.cs
+++ Library.Tests/Scenarios/Scenario03ScanBook.cs
@@ -10,6 +10,7 @@ using Library.Entities;
 using Library.Hardware;
 using Library.Interfaces.Controllers.Borrow;
 using Library.Interfaces.Daos;
+using Library.Interfaces.Entities;
 using Library.Interfaces.Hardware;
 using Xunit;
 
@@ -294,6 +295,81 @@ namespace Library.Tests.Scenarios
             Assert.Equal(EBorrowState.SCANNING_BOOKS, controller._state);
         }
 
+        [WpfFact]
+        public void RunScanCountEqualsLoanLimit()
+        {
+            // Some test data initialisation
+            var borrowDate = DateTime.Today;
+            var dueDate = DateTime.Today.AddDays(7);
+
+            var member = _memberDao.AddMember("Jim", "Tulip", "Phone", "Email");
+
+            var existingBook = _bookDao.AddBook("Jim Tulip", "Adventures in Programming", "call number");
+
+            var existingLoan = _loanDao.CreateLoan(member, existingBook, borrowDate, dueDate);
+
+            _loanDao.CommitLoan(existingLoan);
+
+            var book = _bookDao.AddBook("Jim Tulip", "Adventures in Programming 2", "call number");
+
+            // Set up
+            var controller = new BorrowController(_display, _reader, _scanner, _printer,
+                                                        _bookDao, _loanDao, _memberDao);
+
+            controller.initialise();
+            controller.cardSwiped(member.ID);
+
+            controller.scanCount = BookConstants.LOAN_LIMIT - 1;
+
+            // Test Pre-conditions
+            Assert.True(_display.Display.IsEnabled);
+
+            var borrowCtrl = ((BorrowControl)_display.Display);
+            var scanBookCtrl = borrowCtrl._controlDict.Single(c => c.Value is ScanBookControl).Value as ScanBookControl;
+            var confirmingLoansCtrl = borrowCtrl._controlDict.Single(c => c.Value is ConfirmLoanControl).Value as ConfirmLoanControl;
+
+            Assert.NotNull(scanBookCtrl);
+            Assert.True(scanBookCtrl.IsEnabled);
+            Assert.True(scanBookCtrl.cancelButton.IsEnabled);
+            Assert.True(scanBookCtrl.completeButton.IsEnabled);
+
+            Assert.True(!_reader.Enabled);
+            Assert.True(_scanner.Enabled);
+            Assert.Equal(controller, _scanner.Listener);
+
+            Assert.Equal(member, controller._borrower);
+            Assert.Equal(EBorrowState.SCANNING_BOOKS, controller._state);
+
+            // Run use case
+            controller.bookScanned(book.ID);
+
+            // Test Post-conditions
+            Assert.NotNull(confirmingLoansCtrl);
+            Assert.True(confirmingLoansCtrl.IsEnabled);
+            Assert.True(confirmingLoansCtrl.cancelButton.IsEnabled);
+            Assert.True(confirmingLoansCtrl.rejectButton.IsEnabled);
+
+            Assert.True(!_reader.Enabled);
+            Assert.True(!_scanner.Enabled);
+
+            Assert.Equal(BookConstants.LOAN_LIMIT, controller.scanCount);
+
+            Assert.NotNull(controller._loanList);
+            Assert.NotEmpty(controller._loanList);
+            Assert.Equal(1, controller._loanList.Count);
+            Assert.Equal(book, controller._loanList[0].Book);
+            Assert.Equal(member, controller._loanList[0].Borrower);
+
+            Assert.True(scanBookCtrl.pendingLoanBox.Text.Contains(controller._loanList[0].ToString()));
+
+            Assert.NotNull(controller._bookList);
+            Assert.NotEmpty(controller._bookList);
+            Assert.Equal(1, controller._bookList.Count);
+
+            Assert.Equal(book, controller._bookList[0]);
+
+            Assert.Equal(EBorrowState.CONFIRMING_LOANS, controller._state);
+        }
 
         public void Dispose()
             {
